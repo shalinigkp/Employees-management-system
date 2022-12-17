7f8c84428937716e15599b29f8f7c7018f720670
amit <amitgkpup@hotmail.com>
2022-12-17 20:59:05 +0530

    amit

--- frontend/src/pages/Layout.js
+++ frontend/src/pages/Layout.js
@@ -2,55 +2,55 @@ import React, { useState,useEffect } from "react";
 import { Outlet, Link } from "react-router-dom";
 
 const Layout = () => {
-  const[isLoggedIn,setIsLoggedIn]=useState(false)
-  useEffect(()=>{
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-   
-    setInterval(function(){
-      const user= localStorage.getItem('user');
-      if(user){
-        setIsLoggedIn(true)
-      }else{
-        setIsLoggedIn(false)
+  useEffect(() => {
+    setInterval(function () {
+      const user = localStorage.getItem("user");
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
       }
-    },500)
+    }, 500);
+  }, []);
+  const clearLocalStorage = () => {
+    localStorage.removeItem("user");
+  };
 
- },[])
- const clearLocalStorage=()=>{
-  localStorage.removeItem('user')
- }
- 
   return (
     <>
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">EMPLOYEE MANAGEMENT SYSTEM</a>
-    </div>
-       {
-        isLoggedIn?
-        <ul class="nav navbar-nav">
-          <li>
-            <Link to="/" onClick={clearLocalStorage}>Logout</Link>
-          </li>
-          
-        </ul>
-        :<ul class="nav navbar-nav">
-          <li>
-            <Link to="/">Login</Link>
-          </li>
-          <li>
-            <Link to="/register">Register(If not a user)</Link>
-          </li>
-        </ul>
-      }
-  </div>
-</nav>
-
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#">
+              EMPLOYEE MANAGEMENT SYSTEM
+            </a>
+          </div>
+          {isLoggedIn ? (
+            <ul class="nav navbar-nav">
+              <li>
+                <Link to="/" onClick={clearLocalStorage}>
+                  Logout
+                </Link>
+              </li>
+            </ul>
+          ) : (
+            <ul class="nav navbar-nav">
+              <li>
+                <Link to="/">Login</Link>
+              </li>
+              <li>
+                <Link to="/register">Register(If not a user)</Link>
+              </li>
+            </ul>
+          )}
+        </div>
+      </nav>
 
       <Outlet />
     </>
-  )
+  );
 };
 
 export default Layout;
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,99 +1,74 @@
 import React, { useState } from "react";
-import {useNavigate}from 'react-router-dom'
-import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-const Login=()=> {
-    const history=useNavigate()
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-  
-const loginUser =()=>{
-   
-     const data = {
-      'email':email,
-      'password':password,
-  }
+const Login = () => {
+  const history = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-     axios.post('http://localhost:4000/api/login', data)
-     .then(function (response) {
-      console.log(response)
-      if(response.data.status==='ok'){
-        //alert('Successfully user created');
-        localStorage.setItem('user',data.user)
-        history('/dashboard')
-      }else{
-        alert(response.data.error);
-      }
-       
-     })
-     .catch(function (error) {
-         console.log(error);
-     })
-  
-    }
+  const loginUser = () => {
+    const data = {
+      email: email,
+      password: password,
+    };
+
+    axios
+      .post("http://localhost:4000/api/login", data)
+      .then(function (response) {
+        console.log(response);
+        if (response.data.status === "ok") {
+          localStorage.setItem("user", response.data.user);
+          history("/dashboard");
+        } else {
+          alert(response.data.error);
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
   return (
-   <div className='container'>
-   <div className="row justify-content-md-center">
-   <div className="card">
-   <div className="card-header">Login</div>
-   <div className="card-body">
-   <div className='form-group'>
-            <label htmlFor="">User Name</label>
-            <input
-            className='form-control'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          type="email"
-          placeholder="Email"
-        />
-       </div>
+    <div className="container">
+      <div className="row justify-content-md-center">
+        <div className="card">
+          <div className="card-header">Login</div>
+          <div className="card-body">
+            <div className="form-group">
+              <label htmlFor="">User Name</label>
+              <input
+                className="form-control"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                placeholder="Email"
+              />
+            </div>
 
-  <div className='form-group'>
-  <label>Password:</label>
-  <input className='form-control'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          type="password"
-          placeholder="Password"
-        />
-  </div>
-  <div className='form-group mt-2 text-center'>
-            <input type="button" value="Login" className='btn btn-primary' onClick={loginUser}/> 
-           
-       </div>
-   </div>
-   </div>
-   </div>
-   
-   
-  
-   {/* <div className='form-group'>
-            <label htmlFor="">User Name</label>
-            <input
-            className='form-control'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          type="email"
-          placeholder="Email"
-        />
-       </div>
+            <div className="form-group">
+              <label>Password:</label>
+              <input
+                className="form-control"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                placeholder="Password"
+              />
+            </div>
 
-  <div className='form-group'>
-  <label>Password:</label>
-  <input className='form-control'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          type="password"
-          placeholder="Password"
-        />
-  </div>
-  <div className='form-group mt-2 text-center'>
-            <input type="button" value="Login" className='btn btn-primary' onClick={loginUser}/> 
-           
-       </div>
-    */}
+            <div className="form-group mt-2 text-center">
+              <input
+                type="button"
+                value="Login"
+                className="btn btn-primary"
+                onClick={loginUser}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Login;
