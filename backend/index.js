e50c8ed809a4bb28e602f3f3fb474181cfbcecb7
amit <amitgkpup@hotmail.com>
2022-12-17 20:31:08 +0530

    api

--- backend/index.js
+++ backend/index.js
@@ -17,204 +17,166 @@ const Department=require('./models/department')
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
+//---------------Create User----------------
 
-app.post('/api/register',async(req,res)=>{
-    console.log(req.body)
-    try {
-        
-        await User.create({
-            name:req.body.name,
-            email:req.body.email,
-            password:req.body.password,
-        })
-        res.json({status:"ok"})
-    } catch (err) {
-        console.log(err)
-        res.json({status:'error',error:"Duplicate email"})
-    }
-    //res.json({status:'error',error:"Duplicate email"})
-})
-
-app.post('/api/login',async(req,res)=>{
-  
-   
-    const user= await User.findOne({
-           
-            email:req.body.email,
-            
-        })
-        if(!user){
-            return{status:'error',error:"invalid login"}
-        }
-
-        if(req.body.password==user.password){
-         
-            return res.json({status:"ok",user:user.name})
-        }else{
-            return res.json({status:"error",error:'invalid password'})
-        }
-       
-})
-
-app.get('/api/getEmployeeList',async(req,res)=>{
-   
-    try{
-    
-      const employee=await Employee.find()
-  //console.log(employee)
-      return res.json({status:'ok', employeeList:employee})
-    }catch(error){
-  console.log(error)
-  res.json({status:'error',error:"invalid token"})
-    }
-         
-  })
-
-  app.post('/api/addEmployee',async(req,res)=>{
-    console.log(req.body)
-    try {
-        
-        await Employee.create({
-            _id:mongoose.Types.ObjectId(),
-            firstName:req.body.firstName,
-            lastName:req.body.lastName,
-            dateOfBirth:req.body.dateOfBirth,
-            dateOfJoining:req.body.dateOfJoining,
-            salary:req.body.salary,
-            designation:req.body.designation,
-            department:req.body.department,
-        })
-        res.json({status:"ok"})
-    } catch (err) {
-        console.log(err)
-        res.json({status:'error'})
-    }
-   
-})
-
-app.post('/api/updateEmployee',async(req,res)=>{
-    var id = mongoose.Types.ObjectId(req.body.id);
-    try {
-       
-        await Employee.updateOne(
-            {_id:id},
-            {$set:{
-            firstName:req.body.firstName,
-            lastName:req.body.lastName,
-            dateOfBirth:req.body.dateOfBirth,
-            dateOfJoining:req.body.dateOfJoining,
-            salary:req.body.salary,
-            designation:req.body.designation,
-            department:req.body.department}
-        })
-        res.json({status:"ok"})
-    } catch (err) {
-        console.log(err)
-        res.json({status:'error'})
-    }
-   
-})
-
-app.get('/api/deleteEmployeeRecord/:id',async(req,res)=>{
-   
-    try{
-        var id = mongoose.Types.ObjectId(req.params.id);
-    
-      const employee=await Employee.deleteOne({_id:id})
- 
-      return res.json({status:'ok'})
-    }catch(error){
-  console.log(error)
-  res.json({status:'error'})
-    }
-         
-  })
-
-  app.get('/api/getDepartmentList',async(req,res)=>{
-   
-    try{
-    
-      const employee=await Department.find()
-  console.log(employee)
-      return res.json({status:'ok', deptList:employee})
-    }catch(error){
-  console.log(error)
-  res.json({status:'error',error:"invalid token"})
-    }
-         
-  })
-  app.post('/api/addDepartment',async(req,res)=>{
-    console.log(req.body)
-    try {
-        
-        await Department.create({
-           departmentName:req.body.departmentName,
-        })
-        res.json({status:"ok"})
-    } catch (err) {
-        console.log(err)
-        res.json({status:'error'})
-    }
-   
-})
-app.get('/api/getDesignationList',async(req,res)=>{
-   
-    try{
-    
-      const employee=await Designation.find()
-  console.log(employee)
-      return res.json({status:'ok', desgList:employee})
-    }catch(error){
-  console.log(error)
-  res.json({status:'error',error:"invalid token"})
-    }
-         
-  })
-app.post('/api/addDesignation',async(req,res)=>{
-    console.log(req.body)
-    try {
-        
-        await Designation.create({
-   designationName:req.body.designationName,
-           })
-        res.json({status:"ok"})
-    } catch (err) {
-        console.log(err)
-        res.json({status:'error'})
-    }
-   
-})
-
-app.get('/api/quote',async(req,res)=>{
-  const token=req.headers['x-access-token']
-  try{
-    const decoded=jwt.verify(token,'secret123')
-    const email=decoded.email
-    const user=await User.findOne({email:email})
-
-    return res.json({status:'ok', quote:user.quote})
-  }catch(error){
-console.log(error)
-res.json({status:'error',error:"invalid token"})
+app.post("/api/register", async (req, res) => {
+  console.log(req.body);
+  try {
+    await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    res.json({ status: "ok" });
+  } catch (err) {
+    console.log(err);
+    res.json({ status: "error", error: "Duplicate email" });
   }
-       
-})
+});
 
-app.post('/api/quote',async(req,res)=>{
-    const token=req.headers['x-access-token']
-    try{
-      const decoded=jwt.verify(token,'secret123')
-      const email=decoded.email
-      await User.updateOne(
-        {email:email},
-        {$set:{quote:req.body.quote}})
+//--------------Login User----------------
 
-      return res.json({status:'ok'})
-    }catch(error){
-  console.log(error)
-  res.json({status:'error',error:"invalid token"})
+app.post("/api/login", async (req, res) => {
+  const user = await User.findOne({
+    email: req.body.email,
+  });
+  if (!user) {
+    return { status: "error", error: "invalid login" };
+  }
+  if (req.body.password == user.password) {
+    return res.json({ status: "ok", user: user.name });
+  } else {
+    return res.json({ status: "error", error: "invalid password" });
+  }
+});
+
+//---------------Get Employee List--------------------------
+
+app.get("/api/getEmployeeList", async (req, res) => {
+  try {
+    const employee = await Employee.find();
+    return res.json({ status: "ok", employeeList: employee });
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "error", error: "invalid token" });
+  }
+});
+
+//--------------Add Employee-------------------------------
+
+  app.post("/api/addEmployee", async (req, res) => {
+    console.log(req.body);
+    try {
+      await Employee.create({
+        _id: mongoose.Types.ObjectId(),
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        dateOfBirth: req.body.dateOfBirth,
+        dateOfJoining: req.body.dateOfJoining,
+        salary: req.body.salary,
+        designation: req.body.designation,
+        department: req.body.department,
+      });
+      res.json({ status: "ok" });
+    } catch (err) {
+      console.log(err);
+      res.json({ status: "error" });
     }
-         
-  })
+  });
 
-  module.exports = app;
+  //---------------Update Employee---------------
+
+app.post("/api/updateEmployee", async (req, res) => {
+  var id = mongoose.Types.ObjectId(req.body.id);
+  try {
+    await Employee.updateOne(
+      { _id: id },
+      {
+        $set: {
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          dateOfBirth: req.body.dateOfBirth,
+          dateOfJoining: req.body.dateOfJoining,
+          salary: req.body.salary,
+          designation: req.body.designation,
+          department: req.body.department,
+        },
+      }
+    );
+    res.json({ status: "ok" });
+  } catch (err) {
+    console.log(err);
+    res.json({ status: "error" });
+  }
+});
+
+//------------------------Delete Employee----------------
+
+app.get("/api/deleteEmployeeRecord/:id", async (req, res) => {
+  try {
+    var id = mongoose.Types.ObjectId(req.params.id);
+    const employee = await Employee.deleteOne({ _id: id });
+    return res.json({ status: "ok" });
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "error" });
+  }
+});
+
+//--------------Get Department List------------------
+
+  app.get("/api/getDepartmentList", async (req, res) => {
+    try {
+      const employee = await Department.find();
+      console.log(employee);
+      return res.json({ status: "ok", deptList: employee });
+    } catch (error) {
+      console.log(error);
+      res.json({ status: "error", error: "invalid token" });
+    }
+  });
+
+//--------------Add Department--------------------
+
+  app.post("/api/addDepartment", async (req, res) => {
+    console.log(req.body);
+    try {
+      await Department.create({
+        departmentName: req.body.departmentName,
+      });
+      res.json({ status: "ok" });
+    } catch (err) {
+      console.log(err);
+      res.json({ status: "error" });
+    }
+  });
+
+  //--------------Get Designation---------------------
+
+app.get("/api/getDesignationList", async (req, res) => {
+  try {
+    const employee = await Designation.find();
+    console.log(employee);
+    return res.json({ status: "ok", desgList: employee });
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "error", error: "invalid token" });
+  }
+});
+
+//---------------Add Designation--------------------
+
+app.post("/api/addDesignation", async (req, res) => {
+  console.log(req.body);
+  try {
+    await Designation.create({
+      designationName: req.body.designationName,
+    });
+    res.json({ status: "ok" });
+  } catch (err) {
+    console.log(err);
+    res.json({ status: "error" });
+  }
+});
+
+module.exports = app;
